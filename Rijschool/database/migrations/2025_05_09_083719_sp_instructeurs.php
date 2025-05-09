b2723a4d347d4b36429d0f7a5fb7cd3bf09163b4
ThomasTadesse <328704@student.mboutrecht.nl>
2025-05-09 11:03:19 +0200

    SP done

--- Rijschool/database/migrations/2025_05_09_083719_sp_instructeurs.php
+++ Rijschool/database/migrations/2025_05_09_083719_sp_instructeurs.php
@@ -17,7 +17,6 @@ return new class extends Migration
             CREATE PROCEDURE sp_getAllInstructeurs()
             BEGIN
                 SELECT
-                    id,
                     voornaam,
                     tussenvoegsel,
                     achternaam,
@@ -32,6 +31,93 @@ return new class extends Migration
                     aantal_sterren DESC;
             END
         ');
+
+        DB::unprepared('
+        DROP PROCEDURE IF EXISTS sp_getVoertuigDetails;
+            CREATE PROCEDURE sp_getVoertuigDetails(IN vehicleId INT)
+            BEGIN
+                SELECT
+                    tv.type_voertuig AS TypeVoertuig,
+                    v.type AS Type,
+                    v.kentkenen,
+                    v.bouwjaar,
+                    v.brandstof,
+                    tv.rijbewijscategorie
+                FROM
+                    Voertuigen v
+                INNER JOIN
+                    TypeVoertuig tv ON v.type_voertuig_id = tv.id
+                WHERE
+                    v.is_actief = 1
+                    AND (vehicleId IS NULL OR v.id = vehicleId)
+                ORDER BY
+                    tv.rijbewijscategorie ASC;
+            END
+        ');
+
+        DB::unprepared('
+        DROP PROCEDURE IF EXISTS sp_getAllAvailableVoertuigen;
+            CREATE PROCEDURE sp_getAllAvailableVoertuigen()
+            BEGIN
+                SELECT
+                    tv.type_voertuig AS TypeVoertuig,
+                    v.type AS Type,
+                    v.kentkenen AS Kenteken,
+                    v.bouwjaar AS Bouwjaar,
+                    v.brandstof AS Brandstof,
+                    tv.rijbewijscategorie AS Rijbewijscategorie
+                FROM
+                    Voertuigen v
+                INNER JOIN
+                    TypeVoertuig tv ON v.type_voertuig_id = tv.id
+                WHERE
+                    v.is_actief = 1
+                ORDER BY
+                    tv.rijbewijscategorie ASC;
+            END
+        ');
+        
+        DB::unprepared('
+        DROP PROCEDURE IF EXISTS sp_updateVoertuig;
+            CREATE PROCEDURE sp_updateVoertuig(
+                IN p_voertuig_id INT,
+                IN p_instructeur_id INT,
+                IN p_type_voertuig_id INT,
+                IN p_type VARCHAR(20),
+                IN p_bouwjaar DATE,
+                IN p_brandstof VARCHAR(20),
+                IN p_kenteken VARCHAR(8)
+            )
+            BEGIN
+                -- Update vehicle information
+                UPDATE Voertuigen v
+                SET
+                    v.type_voertuig_id = p_type_voertuig_id,
+                    v.type = p_type,
+                    v.bouwjaar = p_bouwjaar,
+                    v.brandstof = p_brandstof,
+                    v.kentkenen = p_kenteken
+                WHERE
+                    v.id = p_voertuig_id
+                    AND v.is_actief = 1;
+                    
+                -- Update or insert the instructor association
+                IF EXISTS (SELECT 1 FROM VoertuigInstructeurs WHERE voertuig_id = p_voertuig_id AND is_actief = 1) THEN
+                    -- Update existing assignment
+                    UPDATE VoertuigInstructeurs
+                    SET instructeur_id = p_instructeur_id,
+                        datum_toekenning = CURRENT_DATE()
+                    WHERE voertuig_id = p_voertuig_id
+                    AND is_actief = 1;
+                ELSE
+                    -- Create new assignment
+                    INSERT INTO VoertuigInstructeurs
+                        (voertuig_id, instructeur_id, datum_toekenning, is_actief)
+                    VALUES
+                        (p_voertuig_id, p_instructeur_id, CURRENT_DATE(), 1);
+                END IF;
+            END
+        ');
     }
 
     /**
@@ -40,5 +126,8 @@ return new class extends Migration
     public function down(): void
     {
         DB::unprepared('DROP PROCEDURE IF EXISTS sp_getAllInstructeurs');
+        DB::unprepared('DROP PROCEDURE IF EXISTS sp_getVoertuigDetails');
+        DB::unprepared('DROP PROCEDURE IF EXISTS sp_getAllAvailableVoertuigen');
+        DB::unprepared('DROP PROCEDURE IF EXISTS sp_updateVoertuig');
     }
 };
