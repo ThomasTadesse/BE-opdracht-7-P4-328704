247c56aa0838e0db9b1c54432cb6a3674a730af1
ThomasTadesse <328704@student.mboutrecht.nl>
2025-05-14 09:27:38 +0200

    routing && controllers pending

---
+++ Rijschool/app/Http/Controllers/VoertuigController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Voertuig;
+use Illuminate\Http\Request;
+
+class VoertuigController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Voertuig $voertuig)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Voertuig $voertuig)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Voertuig $voertuig)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Voertuig $voertuig)
+    {
+        //
+    }
+}
--- Rijschool/routes/web.php
+++ Rijschool/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\InstructeurController;
+use App\Http\Controllers\VoertuigController;
+
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Arr;
 
@@ -8,6 +11,27 @@ Route::get('/', function () {
     return view('home');
 })->name('home');
 
-// resoure auto
-
 // resource instructeur
+Route::resource('instructeur', InstructeurController::class)
+    ->parameters(['instructeur' => 'instructeur'])
+    ->names([
+        'index' => 'instructeur.index',
+        'create' => 'instructeur.create',
+        'store' => 'instructeur.store',
+        'show' => 'instructeur.show',
+        'edit' => 'instructeur.edit',
+        'update' => 'instructeur.update',
+        'destroy' => 'instructeur.destroy'
+    ]);
+// resource voertuig
+Route::resource('voertuig', VoertuigController::class)
+    ->parameters(['voertuig' => 'voertuig'])
+    ->names([
+        'index' => 'voertuig.index',
+        'create' => 'voertuig.create',
+        'store' => 'voertuig.store',
+        'show' => 'voertuig.show',
+        'edit' => 'voertuig.edit',
+        'update' => 'voertuig.update',
+        'destroy' => 'voertuig.destroy'
+    ]);
