a64e167c76c6ddfe7945aedb085953631bdbecee
ThomasTadesse <328704@student.mboutrecht.nl>
2025-05-15 11:49:04 +0200

    home nav fix

--- Rijschool/resources/views/home.blade.php
+++ Rijschool/resources/views/home.blade.php
@@ -7,5 +7,7 @@
         <h1 class="text-4xl font-bold mb-4">Rijschool</h1>
         <br>
         <a href="{{ route('instructeur.index') }}" class="bg-sky-400 hover:bg-sky-300 text-white font-bold py-2 px-4 rounded">Instructeurs in dienst</a>
+        <br>
+        <a href="{{ route('voertuig.index') }}" class="bg-rose-400 hover:bg-rose-300 text-white font-bold py-2 px-4 rounded">Voertuigen in beslag</a>
     </div>
 </x-layout>
